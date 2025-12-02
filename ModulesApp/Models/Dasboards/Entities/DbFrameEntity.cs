6ec650b84adbd3399bf0d5a1f5e2b52c0e03c084
JanVych <132760222+JanVych@users.noreply.github.com>
2025-12-02 07:43:46 +0100

    update

--- ModulesApp/Models/Dasboards/Entities/DbFrameEntity.cs
+++ ModulesApp/Models/Dasboards/Entities/DbFrameEntity.cs
@@ -1,5 +1,31 @@
-﻿namespace ModulesApp.Models.Dasboards.Entities;
+﻿using ModulesApp.Helpers;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModulesApp.Models.Dasboards.Entities;
 
 public class DbFrameEntity: DbDashboardEntity  
 {
+    [NotMapped]
+    public bool IsCustomWidth { get; set; } = false;
+
+    [NotMapped]
+    public int MaxWidth { get; set; } = 800;
+
+    public override void LoadState()
+    {
+        if (Data.TryGetValue("IsCustomWidth", out var obj))
+        {
+            IsCustomWidth = DataConvertor.ToBool(obj);
+        }
+        if (Data.TryGetValue("MaxWidth", out var maxWidthObj))
+        {
+            MaxWidth = DataConvertor.ToInt32(maxWidthObj);
+        }
+    }
+
+    public override void SaveToData()
+    {
+        Data["IsCustomWidth"] = IsCustomWidth;
+        Data["MaxWidth"] = MaxWidth;
+    }
 }
